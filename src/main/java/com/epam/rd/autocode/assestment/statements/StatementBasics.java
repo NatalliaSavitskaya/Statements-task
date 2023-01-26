cb9652b0a0feeb527177a06e89ed02695a375bea
Iurii Mishcheriakov <eng.syst@gmail.com>
2023-01-26 16:20:11 +0200

    Initial commit

---
+++ src/main/java/com/epam/rd/autocode/assestment/statements/StatementBasics.java
@@ -0,0 +1,30 @@
+package com.epam.rd.autocode.assestment.statements;
+
+public class StatementBasics {
+    public static int addPositive(int value) {
+        // TODO Place your code here
+        return 0;
+    }
+
+    public static int addPositiveSubtractNegative(int value) {
+        // TODO Place your code here
+        return 0;
+    }
+
+    public static int addPositiveSubtractNegativeReplaceZero(int value) {
+        // TODO Place your code here
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addPositive(5));
+        System.out.println(addPositive(-5));
+        System.out.println(addPositive(0));
+        System.out.println(addPositiveSubtractNegative(5));
+        System.out.println(addPositiveSubtractNegative(-5));
+        System.out.println(addPositiveSubtractNegative(0));
+        System.out.println(addPositiveSubtractNegativeReplaceZero(5));
+        System.out.println(addPositiveSubtractNegativeReplaceZero(-5));
+        System.out.println(addPositiveSubtractNegativeReplaceZero(0));
+    }
+}
